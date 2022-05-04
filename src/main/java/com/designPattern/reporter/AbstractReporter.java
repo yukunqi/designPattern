6c257e63597e2dbe449b7967a56af7cc3c536a33
harris <372563150@qq.com>
2022-05-04 11:40:35 +0800

    性能优化 ---- 分段统计 解决当时间区间过大的时候 导致内存膨胀的问题

--- src/main/java/com/designPattern/reporter/AbstractReporter.java
+++ src/main/java/com/designPattern/reporter/AbstractReporter.java
@@ -6,6 +6,7 @@ import com.designPattern.RequestStat;
 import com.designPattern.storage.MetricsStorage;
 import com.designPattern.viewer.StatViewer;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,7 +16,7 @@ public abstract class AbstractReporter {
     private Aggregator aggregator;
     private StatViewer viewer;
 
-
+    private static final long TEN_MINUTES = 10 * 60 * 1000;
 
     public AbstractReporter(MetricsStorage metricsStorage, Aggregator aggregator, StatViewer viewer) {
         this.metricsStorage = metricsStorage;
@@ -23,23 +24,82 @@ public abstract class AbstractReporter {
         this.viewer = viewer;
     }
 
-
     protected void doReport(long startTimeInMillis, long endTimeInMillis) {
         long durationInMillis = endTimeInMillis - startTimeInMillis;
-        Map<String, List<RequestInfo>> requestInfos = metricsStorage.getRequestInfos(startTimeInMillis, endTimeInMillis);
-        Map<String, RequestStat> requestStats = aggregator.aggregate(requestInfos, durationInMillis);
-        viewer.output(requestStats, startTimeInMillis, endTimeInMillis);
+
+        long start_pos = startTimeInMillis;
+        Map<String,List<RequestStat>> requestStatListMap = new HashMap<>();
+
+        while (start_pos < endTimeInMillis){
+            long end_pos = start_pos + TEN_MINUTES;
+            if (end_pos > endTimeInMillis){
+                end_pos = endTimeInMillis;
+            }
+
+            Map<String, List<RequestInfo>> requestInfos = metricsStorage.getRequestInfos(start_pos, end_pos);
+            Map<String, RequestStat> requestStats = aggregator.aggregate(requestInfos, TEN_MINUTES);
+
+            addStat(requestStatListMap,requestStats);
+
+            start_pos = end_pos;
+        }
+
+
+        Map<String, RequestStat> requestStatMap = aggregateFromSegment(requestStatListMap, durationInMillis);
+        viewer.output(requestStatMap, startTimeInMillis, endTimeInMillis);
     }
 
-    protected void doReportByApi(String apiName,long startTimeInMillis, long endTimeInMillis) {
-        long durationInMillis = endTimeInMillis - startTimeInMillis;
-        List<RequestInfo> requestInfos = metricsStorage.getRequestInfos(apiName, startTimeInMillis, endTimeInMillis);
+    private void addStat(Map<String,List<RequestStat>> requestStatListMap,Map<String, RequestStat> requestStatMap){
+        for (Map.Entry<String,RequestStat> entry : requestStatMap.entrySet()){
+            String apiName = entry.getKey();
+            RequestStat requestStat = entry.getValue();
 
-        Map<String,List<RequestInfo>> map = new HashMap<>();
-        map.put(apiName,requestInfos);
+            List<RequestStat> statList = requestStatListMap.computeIfAbsent(apiName, (key) -> {
+                List<RequestStat> requestStatList = new ArrayList<>();
+                requestStatList.add(requestStat);
+                return requestStatList;
+            });
 
-        Map<String, RequestStat> requestStats = aggregator.aggregate(map, durationInMillis);
-        viewer.output(requestStats, startTimeInMillis, endTimeInMillis);
+            statList.add(requestStat);
+        }
+    }
+
+    private Map<String, RequestStat> aggregateFromSegment(Map<String,List<RequestStat>> requestStatListMap,long durationMills){
+
+        Map<String,RequestStat> requestStatMap = new HashMap<>();
+
+        for (Map.Entry<String,List<RequestStat>> entry : requestStatListMap.entrySet()){
+            String apiName = entry.getKey();
+            List<RequestStat> requestStatList = entry.getValue();
+
+            double maxResponse = 0;
+            double minResponse = 0;
+            double totalResponse = 0;
+            long count = 0;
+
+            for (RequestStat requestStat : requestStatList){
+                maxResponse = Math.max(maxResponse,requestStat.getMaxResponseTime());
+                minResponse = Math.min(minResponse,requestStat.getMinResponseTime());
+                count += requestStat.getCount();
+                totalResponse += requestStat.getCount() * requestStat.getAvgResponseTime();
+            }
+
+            double avgResponse = totalResponse / count;
+            long tps =  count / (durationMills * 1000);
+
+            RequestStat fromSegment = new RequestStat();
+            fromSegment.setCount(count);
+            fromSegment.setMaxResponseTime(maxResponse);
+            fromSegment.setMinResponseTime(minResponse);
+            fromSegment.setAvgResponseTime(avgResponse);
+            fromSegment.setTps(tps);
+
+            //todo p999 p99 的计算 需要使用排序算法来实现
+
+            requestStatMap.put(apiName,fromSegment);
+        }
+
+        return requestStatMap;
     }
 
 }
