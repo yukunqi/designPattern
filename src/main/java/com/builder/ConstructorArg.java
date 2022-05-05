9366cbf525d052d4033fa28c0dcd358b64550fa5
harris <372563150@qq.com>
2022-05-05 11:59:14 +0800

    add builder code and readme.md

---
+++ src/main/java/com/builder/ConstructorArg.java
@@ -0,0 +1,62 @@
+package com.builder;
+
+/**
+ * 当 isRef 为 true 的时候，arg 表示 String 类型的 refBeanId，type 不需要设置；
+ * 当 isRef 为 false 的时候，arg、type 都需要设置。
+ * 请根据这个需求，完善 ConstructorArg 类。
+ */
+public class ConstructorArg {
+    private boolean isRef;
+    private Class<?> type;
+    private Object arg;
+
+    private ConstructorArg(){}
+
+    public ConstructorArg(ConstructorArgBuilder builder){
+        this.isRef = builder.isRef;
+        this.type = builder.type;
+        this.arg = builder.arg;
+    }
+
+
+    public static class ConstructorArgBuilder{
+        private boolean isRef;
+        private Class<?> type;
+        private Object arg;
+
+
+        public ConstructorArgBuilder setRef(boolean ref) {
+            isRef = ref;
+            return this;
+        }
+
+        public ConstructorArgBuilder setType(Class<?> type) {
+            this.type = type;
+            return this;
+        }
+
+        public ConstructorArgBuilder setArg(Object arg) {
+            this.arg = arg;
+            return this;
+        }
+
+        public ConstructorArg build(){
+
+            if (isRef){
+                if (!(arg instanceof String)){
+                    throw new IllegalArgumentException("arg is not String when isRef is true");
+                }
+
+                if (type != null){
+                    throw new IllegalArgumentException("don't set type parameter when isRef is true");
+                }
+            }
+
+            if (!isRef && (type == null || arg == null)){
+                throw new IllegalArgumentException("type or arg can not be null");
+            }
+
+            return new ConstructorArg(this);
+        }
+    }
+}
